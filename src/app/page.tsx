18820727fae2711d5d71a78e485a6ad5f53c0225
exolonConfidental <shikharsingh3324@gmail.com>
2025-08-09 00:25:55 +0530

    03:auth-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,96 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
 
 export default function Home() {
+  const { data: session } = authClient.useSession();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const createUser =  () => {
+    authClient.signUp.email(
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        onError: (cxt) => {
+          alert(cxt.error);
+        },
+        onSuccess: () => {
+          alert("Success");
+        },
+      }
+    );
+  };
+  const login =  () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onError: (cxt) => {
+          alert(cxt.error);
+        },
+        onSuccess: () => {
+          alert("Success");
+        },
+      }
+    );
+  };
+  if (session) {
+    return (
+      <div>
+        <h1>{session.user.name}</h1>
+        <Button variant={"destructive"} onClick={() => authClient.signOut()}>
+          Logout
+        </Button>
+      </div>
+    );
+  }
+
   return (
-    <div className="text-4xl bold text-green-500">
-      Hello world!
+    <div className="flex justify-center items-center flex-col h-screen">
+      <div className="flex flex-col gap-4 w-lg">
+        <Input
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={createUser}>Create User</Button>
+      </div>
+      <div className="flex flex-col gap-4 w-lg mt-6">
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={login}>Login</Button>
+      </div>
     </div>
   );
 }
