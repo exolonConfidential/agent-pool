0c045971032d15316f7ae8a31ba47c2c91d459b0
exolonConfidental <shikharsingh3324@gmail.com>
2025-08-21 21:46:54 +0530

    08:tRPC Setup

--- src/modules/home/ui/views/home.tsx
+++ src/modules/home/ui/views/home.tsx
@@ -1,29 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+
 
 export const HomeView = () => {
-  const router = useRouter(); // this is a client side hook 
-  const { data: session } = authClient.useSession();
-
+  const trpc = useTRPC();
+  const {data} = useQuery(trpc.hello.queryOptions({text: "Exolon"}))
   return (
     <div>
-      <h1>{session?.user.name}</h1>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => {
-                router.push("/auth/sign-in");
-              },
-            },
-          })
-        }
-      >
-        Logout
-      </Button>
+      {data?.greeting}
     </div>
   );
 };
